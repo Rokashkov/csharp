43a899b0d7ad797857dc16491c896025f082d5cc
Vladimir <vovalol31@gmail.com>
2023-03-10 20:36:52 +0700

    Добавьте файлы проекта.

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Как вас зовут?");
+                Console.Write("Введите ваше имя: ");
+                string input = Console.ReadLine()!;
+                if (input == "" || input == "Владимир")
+                {
+                    throw new Exception(input);
+                }
+                else
+                {
+                    Console.WriteLine($"Здравствуйте, {input}!");
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Я уверен, что имени \"{ex.Message}\" не существует... Попробуем заново.");
+                Main();
+            }
+        }
+    }
+}
