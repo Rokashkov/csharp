52018521419f14c8b2f8bd04097469cd59f829e4
Vladimir <vovalol31@gmail.com>
2023-03-19 16:23:12 +0700

    done

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main ()
+        {
+            int length = InputHandler.HandleLengthInput();
+            int[] numbers = InputHandler.HandleNumbersInput(length);
+            WritePreMaxValue(numbers);
+        }
+
+        static void WritePreMaxValue (int[] numbers)
+        {
+            Array.Sort(numbers);
+            int max = numbers[numbers.Length - 1];
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                if (numbers[i] < max)
+                {
+                    Console.WriteLine($"Второе наибольшее число в массиве: {numbers[i]}");
+                    return;
+                }
+            }
+            Console.WriteLine("Массив, состоящий только из равных чисел, не имет второго большего числа.");
+        }
+    }
+}
